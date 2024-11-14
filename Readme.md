8a6c15e2edb32a70834059ceeb59c647a899e147
PrateekRajput12 <ficredinn@gmail.com>
2024-11-14 11:28:26 +0530

    addNewPost Done Completely

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# User Work Done Till Here
+ - Now work for post controller
+# Post Controller 
+ - Creating addNewPost 
+- to manage the quality of post or any type of image we use Sharp
+- we used populate to fetch user data 
+- we added post id into user schema 
---
+++ controllers/post.controller.js
@@ -0,0 +1,52 @@
+const express=require('express')
+const Sharp=require("sharp")
+const cloudinary=require("../utils/cloudinary")
+const Post = require('../model/post.model')
+const User = require('../model/user.model')
+const addNewPost=async(req,res)=>{
+    try {
+        const {caption}=req.body
+        const image=req.file
+        const authorId=req.id
+
+        if(!image){
+            return res.status(401).json({
+                message:"Image Required",
+                success:"False"
+            })
+        }
+
+
+        // image Upload
+        const optimixedImageBuffer=await sharp(image.buffer).resize({width:800,height:800,fit:'inside'})
+        .toFormat('jpeg',{quality:80})
+        .toBuffer();
+
+        // buffer to data uri
+
+        const fileUri=`data:image/jpeg;base64,${optimixedImageBuffer.toString('base64')}`
+const cloudResponse=await cloudinary.uploader.upload(fileUri)
+const post =await Post.create({
+    caption,
+    image:cloudResponse.secure_url,
+    author:authorId
+})
+const user= await User.findById(authorId)
+if(user){
+    user.posts.push(post._id)
+    await user.save()
+}
+await post.populate({path:'author',select:'-password'})
+
+return res.status(201).json({
+    message:"New post addes",
+    post,
+    success:true
+
+})
+    } catch (error) {
+        console.log('error in add new post');
+        console.log(error);
+    }
+}
+
